de9d4222ac4c1fc01d6883f456767563d6515248
Jin IGARASHI <wushilan0623@gmail.com>
2021-05-03 09:28:13 +0900

    deleted unncesessary plugins and focusn on terrain 3d...

--- src/config.js
+++ src/config.js
@@ -3,83 +3,13 @@ const version = 0.6;
 module.exports = {
     accessToken : process.env.ACCESSTOKEN,
     attribution : '©WASAC,Ltd.',
-    styles : [
-        { title: 'Terrain', uri: `https://wasac.github.io/mapbox-stylefiles/terrain/style.json?version=${version}`},
-        { title: 'Street', uri: `https://wasac.github.io/mapbox-stylefiles/street/style.json?version=${version}`,}, 
-        { title: 'Satellite', uri: `https://wasac.github.io/mapbox-stylefiles/satellite/style.json?version=${version}`},
-        { title: 'UN Vector', uri: `https://wasac.github.io/mapbox-stylefiles/unvt/style.json?version=${version}`}
-    ],
     center: [30.0291, -2.0032],
     zoom: 9,
-    search:{
-        url: 'https://wasac.github.io/vt/wss.geojson',
-        target: ['wss_name', 'district','po_name'],
-        format: (p) => {return `${p.wss_id}-${p.wss_name}, ${p.po_name}, ${p.district}`},
-        place_type: ['wss'],
-        placeholder: 'Search WSS/PO/District',
-        limit: 10,
-        zoom: 13,
-    },
     popup: {
         target: [
             'handpump','improvedspring','dugwell','solarpump','otherwaterpoint',
             'household','publictap','waterkiosk','industrial','institution','other connection',
             'chamber','reservoir','pumping-station','watersource','pipeline','wss'
         ]
-    },
-    adminBoundary: {
-        url: 'https://wasac.github.io/rw-admin-boundary'
-    },
-    legend:{
-        targets:{
-            'village': 'Village',
-            'village-annotation': 'Village Label',
-            'cell': 'Cell',
-            'cell-annotation': 'Cell Label',
-            'sector': 'Sector',
-            'sector-annotation': 'Sector Label',
-            'district': 'District',
-            'district-annotation': 'District Label',
-            'wss': 'WSS',
-            'wss-annotation': 'WSS Label',
-            'pipeline': 'Pipeline',
-            'pipeline_annotation': 'Pipeline Label',
-            'watersource': 'Water Source',
-            'reservoir': 'Reservoir', 
-            'pumping-station': 'Pumping Station', 
-            'chamber': 'Chamber', 
-            'household': 'Household',
-            'publictap': 'Public Tap',
-            'waterkiosk': 'Water Kiosk',
-            'industrial': 'Industrial',
-            'institution': 'Institution',
-            'other connection': 'Other connection',
-            'handpump': 'Hand Pump',
-            'improvedspring': 'Improved Spring',
-            'dugwell': 'Dug well',
-            'solarpump': 'Solar Pump',
-            'otherwaterpoint': 'Other water point',
-            'parcels': 'Parcels',
-            'parcels_annotation': 'Parcels Label',
-            'contour-line': 'Countour',
-            'contour-label': 'Contour Label',
-            'hillshade': 'Hillshade'
-        },
-        options: {
-            showDefault:false,
-            showCheckbox:true,
-            reverseOrder:true,
-            onlyRendered:true
-        }
-    },
-    elevation: {
-        url: 'https://wasac.github.io/rw-terrain/tiles/{z}/{x}/{y}.png',
-        options: {
-            font: ['Roboto Medium'],
-            fontSize: 12,
-            fontHalo: 1,
-            mainColor: '#263238',
-            haloColor: '#fff',
-        }
     }
 }
--- src/index.js
+++ src/index.js
@@ -1,22 +1,9 @@
 import mapboxgl from 'mapbox-gl';
 import 'mapbox-gl/dist/mapbox-gl.css';
-import MapboxGeocoder from '@mapbox/mapbox-gl-geocoder';
-import '@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css';
-import { MapboxStyleSwitcherControl } from "@watergis/mapbox-gl-style-switcher";
-import "@watergis/mapbox-gl-style-switcher/styles.css"
 import MapboxPopupControl from '@watergis/mapbox-gl-popup';
 import '@watergis/mapbox-gl-popup/css/styles.css';
 import MapboxPitchToggleControl from '@watergis/mapbox-gl-pitch-toggle-control';
 import '@watergis/mapbox-gl-pitch-toggle-control/css/styles.css';
-import MapboxLegendControl from "@watergis/mapbox-gl-legend";
-import '@watergis/mapbox-gl-legend/css/styles.css';
-import { MapboxExportControl } from "@watergis/mapbox-gl-export";
-import '@watergis/mapbox-gl-export/css/styles.css';
-import MapboxElevationControl from "@watergis/mapbox-gl-elevation";
-import '@watergis/mapbox-gl-elevation/css/styles.css';
-import MapboxAdminBoundaryControl from "@wasac/mapbox-gl-admin-boundary";
-import "@wasac/mapbox-gl-admin-boundary/css/styles.css";
-import axios from 'axios';
 import config from './config';
 
 (()=>{
@@ -24,7 +11,7 @@ import config from './config';
 
     const map = new mapboxgl.Map({
         container: 'map',
-        style: config.styles[0].uri,
+        style: 'https://wasac.github.io/mapbox-stylefiles/terrain/style.json',
         center: config.center,
         zoom: config.zoom,
         hash:true,
@@ -34,89 +21,7 @@ import config from './config';
     map.addControl(new mapboxgl.NavigationControl(), 'top-right');
     map.addControl(new mapboxgl.GeolocateControl({positionOptions: {enableHighAccuracy: true},trackUserLocation: true}), 'top-right');
     map.addControl(new MapboxPitchToggleControl({minpitchzoom: 19})); 
-    MapboxStyleSwitcherControl.DEFAULT_STYLE = config.styles[0].title;
-    map.addControl(new MapboxStyleSwitcherControl(config.styles), 'top-right');
-    map.addControl(new MapboxElevationControl(config.elevation.url, config.elevation.options), 'top-right');
-    map.addControl(new MapboxExportControl(), 'top-right');
     map.addControl(new mapboxgl.ScaleControl({maxWidth: 80, unit: 'metric'}), 'bottom-left');
     map.addControl(new mapboxgl.AttributionControl({compact: true,customAttribution: config.attribution}), 'bottom-right');
     if (config.popup)map.addControl(new MapboxPopupControl(config.popup.target));
-    let legendCtrl;
-    if (config.legend){
-        legendCtrl = new MapboxLegendControl(config.legend.targets, config.legend.options);
-        map.addControl(legendCtrl, 'bottom-left')
-    }
-
-    if (config.search){
-        axios.get(config.search.url)
-        .then(res=>{
-            const customerData = res.data;
-            function forwardGeocoder(query) {
-                var matchingFeatures = [];
-                for (var i = 0; i < customerData.features.length; i++) {
-                    var feature = customerData.features[i];
-                    config.search.target.forEach(v=>{
-                        var target = feature.properties[v];
-                        if (!target){
-                            return;
-                        }
-                        // handle queries with different capitalization than the source data by calling toLowerCase()
-                        if ((target.toString().toLowerCase().search(query.toString().toLowerCase()) !== -1)) {
-                            feature['place_name'] = config.search.format(feature.properties);
-                            feature['center'] = feature.geometry.coordinates;
-                            feature['place_type'] = config.search.place_type;
-                            matchingFeatures.push(feature);
-                        }
-                    })
-                }
-                return matchingFeatures;
-            }
-            map.addControl(
-                new MapboxGeocoder({
-                    accessToken: mapboxgl.accessToken,
-                    localGeocoder: forwardGeocoder,
-                    localGeocoderOnly:true,
-                    zoom: config.search.zoom,
-                    placeholder: config.search.placeholder,
-                    mapboxgl: mapboxgl,
-                }),
-                'top-left'
-            );
-            map.addControl(new MapboxAdminBoundaryControl(config.adminBoundary.url), 'top-left');
-        });
-    };
-
-    //set feature-state for WSS
-    let hoverLayers = ['wss', 'pipeline', 'watersource'];
-    let hoveredWssId;
-    map.on('mousemove', 'wss', function(e) {
-        if (e.features.length > 0) {
-        if (hoveredWssId) {
-            hoverLayers.forEach(layer=>{
-                map.setFeatureState(
-                    { source: 'assets', sourceLayer: layer, id: hoveredWssId },
-                    { hover: false }
-                );
-            })
-        }
-        hoveredWssId = e.features[0].id;
-        hoverLayers.forEach(layer=>{
-            map.setFeatureState(
-                { source: 'assets', sourceLayer: layer, id: hoveredWssId },
-                { hover: true }
-            );
-        })
-        }
-    });
-    map.on('mouseleave', 'wss', function() {
-        if (hoveredWssId) {
-            hoverLayers.forEach(layer=>{
-                map.setFeatureState(
-                    { source: 'assets', sourceLayer: layer, id: hoveredWssId },
-                    { hover: false }
-                );
-            })
-        }
-        hoveredWssId = null;
-    });
 })();
